29688a7a24529533bb547222c5c8c8b23898910a
andrejstrogonov <strogonovandrej@hotmail.com>
2026-03-15 16:40:24 +0300

    Fixed errors

--- models/treenode.h
+++ models/treenode.h
@@ -10,32 +10,27 @@ class TreeNode {
 public:
     enum class Type { Root, Object, Transmitter, Spec };
 
-    // Конструкторы
-    explicit TreeNode(const QString &display, Type type, int id, TreeNode *parent = nullptr);
+    explicit TreeNode(const QString &display, Type type = Type::Root, int id = -1, TreeNode *parent = nullptr);
     explicit TreeNode(const QVector<QVariant> &data, TreeNode *parent = nullptr);
     ~TreeNode();
 
-    // Методы для работы с дочерними элементами
     void appendChild(TreeNode *child);
-    void insertChild(int row, TreeNode *child); // Добавлено
-    void removeChild(TreeNode *child);         // Добавлено
+    void insertChild(int row, TreeNode *child);
+    void removeChild(TreeNode *child);
 
     TreeNode *child(int row) const;
     int childCount() const;
     int columnCount() const;
-    
-    // Работа с данными
     QVariant data(int column) const;
     void setData(int column, const QVariant &value);
     
-    // Иерархия
     int row() const;
     TreeNode *parent() const;
     Type type() const;
     int id() const;
 
 private:
-    QVector<QVariant> m_itemData; // Изменено для поддержки нескольких колонок
+    QVector<QVariant> m_itemData;
     Type m_type;
     int m_id;
     TreeNode *m_parent;
