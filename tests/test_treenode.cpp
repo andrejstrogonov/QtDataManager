7ee42315c278086a2771c55702268944670458d8
andrejstrogonov <strogonovandrej@hotmail.com>
2026-03-15 15:43:42 +0300

    Added constructor

--- tests/test_treenode.cpp
+++ tests/test_treenode.cpp
@@ -20,6 +20,29 @@ private slots:
     void testChildCount();
 };
 
+class TreeNode
+{
+public:
+    // The constructor used in your tests
+    explicit TreeNode(const QVector<QVariant> &data, TreeNode *parent = nullptr);
+    ~TreeNode();
+
+    void appendChild(TreeNode *child);
+    void insertChild(int row, TreeNode *child);
+    void removeChild(TreeNode *child);
+
+    TreeNode *child(int row);
+    int childCount() const;
+    int row() const;
+    TreeNode *parent();
+    QVariant data(int column) const;
+
+private:
+    QVector<TreeNode*> m_children;
+    QVector<QVariant> m_itemData;
+    TreeNode *m_parentItem;
+};
+
 void TestTreeNode::testAppendChild()
 {
     QVector<QVariant> data({"root"});
